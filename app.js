131c5c1636f40e8403e1d4a80af0adb5db522b67
Vương Nguyễn Văn <vuong1998hpx@gmail.com>
2021-04-27 01:55:21 +0700

    first

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.set('views', 'view');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log('Server listen on port 3000');
+});
+
+app.get('/', (req, res) => {
+    let data = require('./data/user.json');
+    let title = "Vũ trụ TuMaFimaly - Server 3";
+    let time = "00:00 27/04/2021";
+    let lastUpdate = `Cập nhật lần cuối: ${time}`;
+    let note = `Mỗi ngày mọi người làm nhiệm vụ để tăng điểm cống hiến (Kame, địa ngục, hành tinh ngục tù).\n
+                Mỗi tuần quyên góp quỹ chung 50k Ballz để mở skill vũ trụ.\n
+                Hàng ngày mọi người tập trung đầy đủ tham gia nhiệm vụ vũ trụ 21h
+    `;
+    res.render('index', {users: data, title: title, last: lastUpdate, note: note});
+});
