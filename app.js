593b360364fb40947deb3a593540dd0d9b8f7e1d
vuongnv <vuong1998hpx@gmail.com>
2021-04-27 14:07:00 +0700

    done

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.set('views', 'view');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log(`Server listen on ${port} 5000`);
+    console.log(`Server listen on port ${port} ...`);
 });
 
 app.get('/', (req, res) => {
