a7c6eb902d32d8aef32e9773ba8d1bfb9d1bdc16
vuongnv <vuong1998hpx@gmail.com>
2021-04-28 10:25:11 +0700

    update task

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const fs = require('fs');
 let bodyParser = require('body-parser')
 const port = process.env.PORT || 5000;
+var _ = require('lodash');
 
 app.set('views', 'view');
 app.set('view engine', 'ejs');
@@ -12,12 +13,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-var data = require('./data/user.json');
+var data = require('./public/data/user.json');
 const moment = require('moment');
-var {title} = require('./data/title.json') || "Vũ trụ TuMaFimaly - Server 3";
-var time = "00:00 27/04/2021";
+var {title} = require('./public/data/title.json') || "Vũ trụ TuMaFimaly - Server 3";
+var time = moment().format('MM/DD/YYYY, HH:mm:ss');
 
-var {note} = require('./data/note.json') || '';
+var {note} = require('./public/data/note.json') || '';
 
 app.listen(port, () => {
     console.log(`Server listen on port ${port} ...`);
@@ -25,10 +26,27 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     let lastUpdate = `Cập nhật lần cuối: ${time}`;
-    res.render('index', {users: data, title: title, last: lastUpdate, note: note});
+    data.forEach(item => {
+        item.stt = parseInt(item.stt);
+        item.position = parseInt(item.position);
+        item.scorce = parseInt(item.scorce);
+        item.weekScore = parseInt(item.weekScore);
+        item.ballz = parseInt(item.ballz);
+    })
+    let users = _.orderBy(data, ['stt'], ['asc']);
+    console.log('user ', users);
+    res.render('index', {users: users, title: title, last: lastUpdate, note: note});
 });
 app.get('/edit', (req, res) => {
-    res.render('edit', {users: data, title: title, note: note});
+    data.forEach(item => {
+        item.stt = parseInt(item.stt);
+        item.position = parseInt(item.position);
+        item.scorce = parseInt(item.scorce);
+        item.weekScore = parseInt(item.weekScore);
+        item.ballz = parseInt(item.ballz);
+    });
+    let users = _.orderBy(data, ['stt'], ['asc']);
+    res.render('edit', {users: users, title: title, note: note});
 });
 
 app.post('/update', (req, res) => {
@@ -39,10 +57,12 @@ app.post('/update', (req, res) => {
     data = JSON.parse(data1);
     title = title1;
     note = note1;
-    time = moment().format('MM/DD/YYYY, HH:mm:ss');;
-    fs.writeFileSync('./data/user.json', data1);
-    fs.writeFileSync('./data/title.json', JSON.stringify({title:title1}));
-    fs.writeFileSync('./data/note.json', JSON.stringify({node: note1}));
+    let users = _.orderBy(data, ['stt'], ['asc']);
+    data = users;
+    time = moment().format('MM/DD/YYYY, HH:mm:ss');
+    fs.writeFileSync('./public/data/user.json', JSON.stringify(users));
+    fs.writeFileSync('.//public/data/title.json', JSON.stringify({title:title1}));
+    fs.writeFileSync('./public/data/note.json', JSON.stringify({node: note1}));
     res.send({status: true});
 });
 
