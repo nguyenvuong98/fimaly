65b736872959f1412dab31c680300111ea6c0687
Vương Nguyễn Văn <vuong1998hpx@gmail.com>
2021-04-30 19:02:41 +0700

    done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 let bodyParser = require('body-parser')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 var _ = require('lodash');
 
 app.set('views', 'view');
@@ -25,44 +25,10 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
-    let lastUpdate = `Cập nhật lần cuối: ${time}`;
-    data.forEach(item => {
-        item.stt = parseInt(item.stt);
-        item.position = parseInt(item.position);
-        item.scorce = parseInt(item.scorce);
-        item.weekScore = parseInt(item.weekScore);
-        item.ballz = parseInt(item.ballz);
-    })
-    let users = _.orderBy(data, ['stt'], ['asc']);
-    console.log('user ', users);
-    res.render('index', {users: users, title: title, last: lastUpdate, note: note});
+    res.render('index');
 });
 app.get('/edit', (req, res) => {
-    data.forEach(item => {
-        item.stt = parseInt(item.stt);
-        item.position = parseInt(item.position);
-        item.scorce = parseInt(item.scorce);
-        item.weekScore = parseInt(item.weekScore);
-        item.ballz = parseInt(item.ballz);
-    });
-    let users = _.orderBy(data, ['stt'], ['asc']);
-    res.render('edit', {users: users, title: title, note: note});
+    res.render('edit');
 });
 
-app.post('/update', (req, res) => {
-    let data1 =  req.body.users;
-    let title1 = req.body.title;
-    let note1 = req.body.note;
-    //res.send({data, title, note});
-    data = JSON.parse(data1);
-    title = title1;
-    note = note1;
-    let users = _.orderBy(data, ['stt'], ['asc']);
-    data = users;
-    time = moment().format('MM/DD/YYYY, HH:mm:ss');
-    fs.writeFileSync('./public/data/user.json', JSON.stringify(users));
-    fs.writeFileSync('.//public/data/title.json', JSON.stringify({title:title1}));
-    fs.writeFileSync('./public/data/note.json', JSON.stringify({node: note1}));
-    res.send({status: true});
-});
 
